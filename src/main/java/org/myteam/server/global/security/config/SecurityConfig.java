dc50bf0cf5d4b2784ada36d64e6693c2c7a8d60e
POKUDING <yoseb95@gmail.com>
2024-11-28 12:38:53 +0900

    CM-46 Feat: Jwt 인증필터 필터체인에 적용

--- src/main/java/org/myteam/server/global/security/config/SecurityConfig.java
+++ src/main/java/org/myteam/server/global/security/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package org.myteam.server.global.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.myteam.server.global.jwt.JwtProvider;
+import org.myteam.server.global.security.filter.TokenAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -9,10 +13,15 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.HstsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
