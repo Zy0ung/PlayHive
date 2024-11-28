a724166b4f0d47d9af8f883656757632e14f133e
POKUDING <yoseb95@gmail.com>
2024-11-28 11:02:47 +0900

    CM-46 feat: 멤버 entity 개발

---
+++ src/main/java/org/myteam/server/member/entity/Member.java
@@ -0,0 +1,48 @@
+package org.myteam.server.member.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_members")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String role;
+
+    //TODO: 적은 확률로 충돌이 일어나는 상황을 방지하는 로직 필요
+    @Column(nullable = false, updatable = false, unique = true, columnDefinition = "BINARY(16)")
+    private UUID publicId = UUID.randomUUID();
+
+    @Builder
+    private Member(String username, String password, String email, String role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
