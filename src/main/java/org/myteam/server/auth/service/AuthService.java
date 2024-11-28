b36efcf5b72ca310cc47396cc3cf390c75bd78b6
POKUDING <yoseb95@gmail.com>
2024-11-28 11:37:03 +0900

    CM-46 Chore: 주석 추가

--- src/main/java/org/myteam/server/auth/service/AuthService.java
+++ src/main/java/org/myteam/server/auth/service/AuthService.java
@@ -23,13 +23,16 @@ public class AuthService {
     @Transactional(readOnly = true)
     public TokenResponseDto login(LoginRequestDto requestDto) {
 
+        //유저확인
         Member member = memberRepository.findByUsername(requestDto.getUsername())
                 .orElseThrow(() -> new RuntimeException("Member not found"));
 
+        //비밀번호 확인
         if (!passwordEncoder.matches(requestDto.getPassword(), member.getPassword())) {
             throw new RuntimeException("Password not matched");
         }
 
+        //토큰 생성
         String accessToken = jwtProvider.generateToken(Duration.ofHours(1), member.getPublicId(), member.getRole());
         String refreshToken = jwtProvider.generateToken(Duration.ofDays(7), member.getPublicId(), member.getRole());
 
@@ -38,9 +41,11 @@ public class AuthService {
 
     @Transactional
     public TokenResponseDto signup(SignupRequestDto requestDto) {
+        //회원 정보 저장
         Member member = requestDto.toEntity(passwordEncoder);
         memberRepository.save(member);
 
+        //토큰 생성
         String accessToken = jwtProvider.generateToken(Duration.ofHours(1), member.getPublicId(), member.getRole());
         String refreshToken = jwtProvider.generateToken(Duration.ofDays(7), member.getPublicId(), member.getRole());
 
