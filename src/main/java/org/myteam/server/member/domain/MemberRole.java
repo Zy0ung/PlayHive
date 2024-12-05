ab2d869455edc112210c46c1990af114d6b53ba7
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2024-12-05 17:42:01 +0900

    MemberRole 권한 ENUM 추가  - ADMIN, USER 권한

---
+++ src/main/java/org/myteam/server/member/domain/MemberRole.java
@@ -0,0 +1,11 @@
+package org.myteam.server.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum MemberRole {
+    USER("일반회원"), ADMIN("관리자");
+    private String value;
+}
