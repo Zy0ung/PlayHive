9c4b6241c25e5467ecca86709a218c44ee3f2a36
POKUDING <yoseb95@gmail.com>
2024-12-11 21:37:50 +0900

    CM-46 Feat: AuthController ResponseEntity 응답으로 변경

--- src/main/java/org/myteam/server/auth/controller/AuthController.java
+++ src/main/java/org/myteam/server/auth/controller/AuthController.java
@@ -1,10 +1,13 @@
 package org.myteam.server.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.myteam.server.auth.dto.LoginRequestDto;
 import org.myteam.server.auth.dto.SignupRequestDto;
 import org.myteam.server.auth.dto.TokenResponseDto;
 import org.myteam.server.auth.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,13 +20,14 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(summary = "로그인", security = {})
     @PostMapping("/login")
-    public TokenResponseDto login(@RequestBody LoginRequestDto requestDto) {
-        return authService.login(requestDto);
+    public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequestDto requestDto) {
+        return ResponseEntity.ok(authService.login(requestDto));
     }
 
     @PostMapping("/signup")
-    public TokenResponseDto signup(@RequestBody SignupRequestDto requestDto) {
-        return authService.signup(requestDto);
+    public ResponseEntity<TokenResponseDto> signup(@RequestBody SignupRequestDto requestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.signup(requestDto));
     }
 }
