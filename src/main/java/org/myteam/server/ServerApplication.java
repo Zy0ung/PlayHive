93cd6b2b1c704f88aa3aca6b82bd17636a52015d
net79736 <34103253+net79736@users.noreply.github.com>
2025-01-10 02:14:53 +0900

    feat : @EnableJpaAuditing 적용

--- src/main/java/org/myteam/server/ServerApplication.java
+++ src/main/java/org/myteam/server/ServerApplication.java
@@ -1,17 +1,27 @@
 package org.myteam.server;
 
+import org.myteam.server.global.config.AuditorAwareImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
 
 }
--- src/main/java/org/myteam/server/global/domain/PlayHive.java
+++ src/main/java/org/myteam/server/global/domain/PlayHive.java
@@ -1,9 +1,11 @@
 package org.myteam.server.global.domain;
 
 public class PlayHive {
+    public static final String NAME = "PLAYHIVE";
     public static final String PLAYHIVE_HOME = "playhive.home";
     public static final String CLIENT_ID_KEY = "PLAYHIVE_CLIENT_ID";
     public static final String CLIENT_SECRET_KEY = "PLAYHIVE_CLIENT_SECRET";
 
-    private PlayHive() {}
+    private PlayHive() {
+    }
 }
