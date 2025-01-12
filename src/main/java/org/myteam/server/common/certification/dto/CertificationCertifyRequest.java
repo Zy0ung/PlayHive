482c7419e0844d9edc9026f47316043df1a5729b
net79736 <34103253+net79736@users.noreply.github.com>
2025-01-13 04:20:06 +0900

    feat : 이메일 인증 관련 DTO 수정

--- src/main/java/org/myteam/server/common/certification/dto/CertificationCertifyRequest.java
+++ src/main/java/org/myteam/server/common/certification/dto/CertificationCertifyRequest.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 
 @Getter
 public class CertificationCertifyRequest {
-    @Pattern(regexp = "^[a-zA-Z가-힣]{1,40}$", message = "한글/영문 1~40자 이내로 작성해주세요")
+    @Pattern(regexp = "^[0-9a-zA-Z]+@[0-9a-zA-Z]+(\\.[a-zA-Z]{2,3}){1,2}$", message = "이메일 형식으로 작성해주세요")
     private String email;
     @NotBlank
     private String code;
